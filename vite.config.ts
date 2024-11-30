b74efd6c2b9283d24f8757506fa719ff8c2961ce
Alessandro Amella <info@bitrey.it>
2024-11-30 13:23:58 +0100

    port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    port: 1114,
     proxy: {
       '/v1': {
         target: 'http://localhost:1447',
