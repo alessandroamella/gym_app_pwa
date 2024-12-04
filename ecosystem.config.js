f91727d25317fa59f5b6ca75141912217ea47778
Alessandro Amella <info@bitrey.it>
2024-12-04 03:01:55 +0100

    Add pm2 file

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'Gym app backend',
+      script: 'pnpm',
+      args: 'build && pnpm start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 1447,
+        BACKEND_URL: 'https://gym-app-backend.bitrey.it/v1',
+      },
+      interpreter: 'bash',
+    },
+  ],
+};
